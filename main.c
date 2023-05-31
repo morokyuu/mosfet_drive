8ae8eaa5cd354a8fb0d5d02860ed827e3d332528
nano <nano@example.com>
2023-05-31 23:20:54 +0900

    pin1 low high

--- main.c
+++ main.c
@@ -3,7 +3,7 @@
 #include "hardware/gpio.h"
 #include "pico/binary_info.h"
 
-const uint LED_PIN = PICO_DEFAULT_LED_PIN;
+const uint LED_PIN = 25;
 
 int main(void){
     bi_decl(bi_program_description("This is a mosfet drive test"));
@@ -12,10 +12,24 @@ int main(void){
 
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    gpio_init(0);
+    gpio_init(1);
+    gpio_init(2);
+    gpio_init(3);
+    gpio_set_dir(0, GPIO_OUT);
+    gpio_set_dir(1, GPIO_OUT);
+    gpio_set_dir(2, GPIO_OUT);
+    gpio_set_dir(3, GPIO_OUT);
+
     while(1){
         gpio_put(LED_PIN, 1);
+        gpio_put(1, 1);
+        //gpio_put_all(0x0000000F);
         sleep_ms(200);
         gpio_put(LED_PIN, 0);
+        gpio_put(1, 0);
+        //gpio_put_all(0x00000000);
         sleep_ms(200);
     }
 }
