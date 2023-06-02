6e8c6d1f4fcf2617a32d010b6c9110ced88deb53
nano <nano@example.com>
2023-06-02 22:55:43 +0900

    pwm for gpio2 pin

--- main.c
+++ main.c
@@ -2,8 +2,25 @@
 #include <pico/stdlib.h>
 #include "hardware/gpio.h"
 #include "pico/binary_info.h"
+#include "hardware/pwm.h"
 
 const uint LED_PIN = 25;
+const uint PWM_PORT = 2;
+const uint PWM_PERIOD = 3300;
+
+void init(void){
+    uint slice_num = pwm_gpio_to_slice_num(PWM_PORT);
+    gpio_set_function(PWM_PORT,GPIO_FUNC_PWM);
+
+    pwm_config config;
+    pwm_config_set_phase_correct(&config, false);
+    pwm_config_set_clkdiv_int(&config, 2);
+    pwm_config_set_clkdiv_mode(&config, PWM_DIV_FREE_RUNNING);
+    pwm_config_set_output_polarity(&config, false, false);
+    pwm_config_set_wrap(&config, PWM_PERIOD);
+    pwm_init(slice_num, &config, true);
+}
+
 
 int main(void){
     bi_decl(bi_program_description("This is a mosfet drive test"));
@@ -12,24 +29,15 @@ int main(void){
 
     gpio_init(LED_PIN);
     gpio_set_dir(LED_PIN, GPIO_OUT);
-
-    gpio_init(0);
-    gpio_init(1);
-    gpio_init(2);
-    gpio_init(3);
-    gpio_set_dir(0, GPIO_OUT);
-    gpio_set_dir(1, GPIO_OUT);
-    gpio_set_dir(2, GPIO_OUT);
-    gpio_set_dir(3, GPIO_OUT);
+    
+    init();
 
     while(1){
         gpio_put(LED_PIN, 1);
-//        gpio_put(1, 1);
-        gpio_put_all(0x0000000F);
+        pwm_set_gpio_level(PWM_PORT, (int)(PWM_PERIOD*0.3));
         sleep_ms(200);
         gpio_put(LED_PIN, 0);
-//        gpio_put(1, 0);
-        gpio_put_all(0x00000000);
+        pwm_set_gpio_level(PWM_PORT, (int)(PWM_PERIOD*0.5));
         sleep_ms(200);
     }
 }
